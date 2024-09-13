9c43d4368cd3c8bbd902732028cd463d29305ed6
Rika Kamila <rikakamila1717@gmail.com>
2024-09-14 01:35:55 +0700

    add menu about

--- homepage/homepageuser.js
+++ homepage/homepageuser.js
@@ -1,6 +1,9 @@
-// ambil data dari local storage
+//ambil data dari local storage
 //pengecekan apakah data user di local storage ada atau enggak
 //kalau ada ambil datanya
 //ambil namanya
 //panggil navbar home page yang belum login diganti ke navbar homepage yang sudah login dengan menampilkan nama user (DOM)
 //Arahkan pilihan ke profil user/logout
+
+
+
